fb61855d01191aca785106b3557884edf8a4b2c2
Kurt Logan <kurt.logan@hotmail.co.uk>
2017-02-04 15:56:14 +0000

    added push button endpoint

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+var bodyParser = require('body-parser')
+var cors = require('cors')
 var express = require('express')
 
 var app = express()
+
+app.use(bodyParser.json())
 
 app.get('/', (request, response) => {
     response.send("Server active!")
 })
 
-app.post('/pushbutton', () => {
-    console.log('button pushed')
+app.post('/pushbutton', cors(), (request, response) => {
+    console.log('button pushed:', request.body.value)
+    response.send(`button pushed: ${request.body.value}`)
 })
 
 app.listen(3000, () => {
