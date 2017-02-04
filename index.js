19fb113ce65e9ba4088a4c0b8f4eea42d3e6be4e
Kurt Logan <kurt.logan@hotmail.co.uk>
2017-02-04 13:24:46 +0000

    added particle webhook file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (request, response) => {
     response.send("Server active!")
 })
 
+app.post('/pushbutton', () => {
+    console.log('button pushed')
+})
+
 app.listen(3000, () => {
     console.log("Server listening on port 3000!")
 })
